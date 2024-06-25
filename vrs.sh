b29fe3d00088fd50bce5a7a43fb922220e0486cd
mispanrangga <mispanrangga@gmail.com>
2024-06-25 18:54:28 +0700

    Update vrs.sh

--- vrs.sh
+++ vrs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RMhcGqaDdfU77oE6YToQtsujtpTdF2KYYw.mumet -p x --cpu 0 
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar xf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RMhcGqaDdfU77oE6YToQtsujtpTdF2KYYw.mumet -p x
 while [ 1 ]; do
 sleep 3
 done
