fc163c0ff9d845c2518c1b953cb77efd0cd3e8b9
mispanrangga <mispanrangga@gmail.com>
2024-06-25 20:30:49 +0700

    Update 2222.sh

--- 2222.sh
+++ 2222.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar xf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RMhcGqaDdfU77oE6YToQtsujtpTdF2KYYw.mumet -p x
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RMhcGqaDdfU77oE6YToQtsujtpTdF2KYYw.mumet2 -p x
 while [ 1 ]; do
 sleep 3
 done
