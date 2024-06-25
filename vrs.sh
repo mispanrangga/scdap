3b694d9c1cffddad19e04c104472e99da3e388b5
mispanrangga <mispanrangga@gmail.com>
2024-06-25 19:02:07 +0700

    Update vrs.sh

--- vrs.sh
+++ vrs.sh
@@ -4,7 +4,3 @@ sudo apt install screen -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar xf hellminer_linux64.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RMhcGqaDdfU77oE6YToQtsujtpTdF2KYYw.mumet -p x
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
